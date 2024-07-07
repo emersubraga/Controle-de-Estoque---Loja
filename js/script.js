fd745c7e623e2dae14657adf9965dc094f06a06e
Emerson Braga <emersonviniciusbraga@gmail.com>
2024-07-06 21:53:20 -0300

    Últimos ajustes.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,100 @@
+
+// Função para adicionar produto em uma lista
+
+function addProduct() {
+    const container = document.getElementById('produtos');
+    const produtoSelect = document.querySelector('select[name="produto[]"]');
+    const quantidadeInput = document.querySelector('input[name="quantidade[]"]');
+    const produto = produtoSelect.value;
+    const quantidade = quantidadeInput.value;
+
+    if (produto !== 'vazio' && quantidade) {
+        const productItem = document.createElement('tr');
+        productItem.className = 'produtos-selecionados';
+
+        productItem.innerHTML = `
+            <td>${produto}</td>
+            <td>${quantidade}</td>
+            <td><a type="button" class="remove-button" onclick="removeProduct(this)">Remover</a></td>
+        `;
+
+        container.appendChild(productItem);
+        produtoSelect.value = 'vazio';
+        quantidadeInput.value = '';
+
+        document.getElementById('table-header').style.display = 'table-header-group';
+    }
+}
+
+// Função para remover produto da lista
+
+function removeProduct(button) {
+    const row = button.parentElement.parentElement;
+    row.remove();
+
+    if (document.getElementById('produtos').children.length === 0) {
+        document.getElementById('table-header').style.display = 'none';
+    }
+}
+
+
+
+//-------------------------------------------------------------------//
+
+
+
+// Função para buscar produtos
+
+function buscaFornecedor() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("buscaInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("fornecedor");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
+
+
+//-------------------------------------------------------------------//
+
+
+// Função para buscar produtos
+
+function buscaProduto() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("buscaInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("produto");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+}
+
+
+//-------------------------------------------------------------------//
+
+
 // Pegue o modal
 var modal = document.getElementById("myModal");
 
